8c8aa8f639fd9c1c73644622759d04e1b2c3a030
jamiel <i@jamiel.net>
2017-10-03 21:05:13 -0400

    updates

--- app/services/askde/AskDESkillService.java
+++ app/services/askde/AskDESkillService.java
@@ -186,7 +186,7 @@ public class AskDESkillService extends BaseAlexaService {
 		if(neighborhoodSlot==null)
 			return errorResponse("A neighborhood was not identified, please restate the neighborhood or try an alternative neighborhood");
 		String neighborhood = null;
-		if(neighborhoodSlot.getResolutions()!=null && neighborhoodSlot.getResolutions().getResolutionAtIndex(0) !=null && neighborhoodSlot.getResolutions().getResolutionAtIndex(0).getValueWrappers().size()<1) {
+		if(neighborhoodSlot.getResolutions()!=null && neighborhoodSlot.getResolutions().getResolutionAtIndex(0) !=null && neighborhoodSlot.getResolutions().getResolutionAtIndex(0).getValueWrappers().size()>0) {
 			Resolution r = neighborhoodSlot.getResolutions().getResolutionAtIndex(0);
 			Logger.info("r is null " + (r==null));
 			Logger.info("size " + r.getValueWrappers().size());
